9d9f5dec311b3ab3c848a25e379d1d91c8abd10c
Dustin Haffner <dh@xix.org>
2019-08-08 00:11:02 +0000

    add required modules to setup()

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='transformers',
-      version='0.1',
-      description="Experimental module for AST transformations.",
-      author='Vladimir Iakovlev',
-      author_email='nvbn.rm@gmail.com',
-      url='https://github.com/nvbn/__transformers__',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples',
-                                      'tests', 'tests.*', 'release']))
+setup(
+    name="transformers",
+    version="0.1",
+    description="Experimental module for AST transformations.",
+    author="Vladimir Iakovlev",
+    author_email="nvbn.rm@gmail.com",
+    url="https://github.com/nvbn/__transformers__",
+    license="MIT",
+    install_requires=["docopt", "astor"],
+    packages=find_packages(
+        exclude=["ez_setup", "examples", "tests", "tests.*", "release"]
+    ),
+)
